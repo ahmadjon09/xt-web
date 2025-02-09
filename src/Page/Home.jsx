bc0152b8b34c0b565f0b2e8564e46b7101a8b6fa
Ahmadjon Mahmudov <154054435+ahmadjon09@users.noreply.github.com>
2025-02-09 21:35:32 +0500

    Update Home.jsx

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -43,23 +43,7 @@ export const Home = () => {
     setSuccess(null);
 
     try {
-    const caption = `🥶 *${formData.name.toUpperCase()} uchun aksiya!*
-
-          💰 *Narxi:* ${formData.price} so‘m 😇
-          
-          📏 *O'lcham:* ${selectedSize}
-          
-          👕 *Kategoriya:* ${category}
-          
-          🚚 *Dastafka:* Bor✨
-          
-          ✈️ *Yetib borish muddati:* 20 kun
-          
-          *Shoshiling! ✅*
-          
-          📩 Murojaat uchun:/n'@ProgrammWeeb_'
-          👤 Bosh admin:/n '@ItsNoWonder_'
-          `
+      const caption = `🥶 *${formData.name.toUpperCase()} uchun aksiya!*\n\n💰 *Narxi:* ${formData.price} so‘m 😇\n\n📏 *O'lcham:* ${selectedSize}\n\n👕 *Kategoriya:* ${formData.category}\n\n🧑‍🦰 *Kim uchun:* ${formData.gender}\n\n🚚 *Dastafka:* Bor✨\n\n✈️ *Yetib borish muddati:* 20 kun\n\n*Shoshiling! ✅*\n\n📩 Murojaat uchun: '@ProgrammWeeb_'\n👤 Bosh admin: '@ItsNoWonder_'`;
 
       const mediaGroup = await Promise.all(
         formData.images.map(async image => {
@@ -105,6 +89,23 @@ export const Home = () => {
         <h2 className='text-lg font-bold mb-4'>Botga Post Yuborish</h2>
         <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
           <input type='file' name='images' onChange={handleFileChange} multiple className='p-2 bg-[#241b2a] text-white border-2 border-white focus:outline-none' />
+          <div className='flex flex-wrap justify-center gap-2 mt-2'>
+            {formData.images.map((image, index) => (
+              <div
+                key={index}
+                className='relative rounded-lg w-20 h-20 bg-cover bg-center'
+                style={{ backgroundImage: `url(${URL.createObjectURL(image)})` }}
+              >
+                <button
+                  type='button'
+                  onClick={() => handleRemoveImage(index)}
+                  className='absolute cursor-pointer top-0 right-0 text-red-500 hover:text-black rounded-full w-5 h-5 flex items-center justify-center'
+                >
+                  <X />
+                </button>
+              </div>
+            ))}
+          </div>
           <input type='text' name='name' placeholder='Mahsulot nomi' value={formData.name} onChange={handleChange} className='p-2 bg-[#241b2a] text-white border-2 border-white focus:outline-none' required />
           <input type='number' name='price' placeholder='Narx' value={formData.price} onChange={handleChange} className='p-2 bg-[#241b2a] text-white border-2 border-white focus:outline-none' required />
           <div className='flex w-auto justify-between max-w-[272px] gap-2'>
